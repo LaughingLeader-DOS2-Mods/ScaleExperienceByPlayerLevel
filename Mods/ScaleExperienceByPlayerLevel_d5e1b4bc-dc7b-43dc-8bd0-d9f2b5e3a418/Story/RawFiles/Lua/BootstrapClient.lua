d921894bde19909359822b03aeda908d1538d58f
LaughingLeader <laughingleader@gmail.com>
2020-03-29 10:57:19 -0500

    Junctioned project folders, added .gitignore, added attributes, added license

---
+++ Mods/ScaleExperienceByPlayerLevel_d5e1b4bc-dc7b-43dc-8bd0-d9f2b5e3a418/Story/RawFiles/Lua/BootstrapClient.lua
@@ -0,0 +1,57 @@
+---@param key string
+---@param fallback number
+---@return number
+local function GetExtraDataValue(key, fallback)
+	local val = Ext.ExtraData[key]
+	if val ~= nil then return val end
+	return fallback
+end
+
+function LLXPSCALE_Ext_GetScaledExperience(gain, level)
+	local levelCap = math.floor(GetExtraDataValue("LevelCap", 35))
+	if gain ~= nil and gain > 0 then
+		if Ext.IsDeveloperMode() then Ext.Print("[LLXPSCALE:BootstrapClient.lua:GetScaledExperience] gain(" .. tostring(gain) .. ") level(" .. tostring(level) .. "/".. tostring(levelCap) ..")") end
+		if levelCap == nil or levelCap <= 0 then levelCap = 1 end
+		if level >= levelCap then return 0 end
+
+		local quadBaseActPart = level
+		if level > 8 then quadBaseActPart = 8 end
+		local quadraticActPart = level - quadBaseActPart;
+		local actPartQuad = quadBaseActPart * (quadBaseActPart + 1)
+		local actMod = 0.0
+		if quadraticActPart > 0 then
+			if quadraticActPart == 2 then
+				actMod = 1.9320999;
+			else
+				actMod = 1.39 ^ quadraticActPart
+				local x = (actPartQuad * actMod)
+				-- round  x + 0.5 - (x + 0.5) % 1
+				actPartQuad = x + 0.5 - (x + 0.5) % 1
+			end
+		end
+		local xp = math.floor((10 * gain * actPartQuad + 24) / 25) * 25
+
+		if Ext.IsDeveloperMode() then 
+			Ext.Print("	[BootstrapClient.lua:LLXPSCALE_ExperienceScale] quadBaseActPart(" .. tostring(quadBaseActPart) .. ") quadraticActPart(" .. tostring(quadraticActPart)..") actPartQuad(" .. tostring(actPartQuad)..") actMod(" .. tostring(actMod)..") actPartQuad(" .. tostring(actPartQuad)..")")
+			Ext.Print("[BootstrapClient.lua:LLXPSCALE_ExperienceScale] xp(".. tostring(xp) ..")")
+		end
+		return xp
+	end
+	return 0
+end
+
+local function LLXPSCALE_Client_InitializeModule()
+	Ext.StatSetLevelScaling("Character", "Gain", function(gain,level) return 0 end)
+	Ext.Print("[LLXPSCALE:BootstrapClient.lua:LLXPSCALE_ModuleLoading] Registered Gain scale override function.")
+end
+
+local function LLXPSCALE_Client_ModuleLoading()
+	LLXPSCALE_Client_InitializeModule()
+end
+
+local function LLXPSCALE_Client_ModuleResume()
+	LLXPSCALE_Client_InitializeModule()
+end
+
+Ext.RegisterListener("ModuleLoading", LLXPSCALE_Client_ModuleLoading)
+Ext.RegisterListener("ModuleResume", LLXPSCALE_Client_ModuleResume)
---
+++ Mods/ScaleExperienceByPlayerLevel_d5e1b4bc-dc7b-43dc-8bd0-d9f2b5e3a418/Story/RawFiles/Lua/BootstrapServer.lua
@@ -0,0 +1,157 @@
+local function FlattenTable(tbl)
+	local result = { }
+		
+	local function flatten(tbl)
+		for _, v in ipairs(tbl) do
+			if type(v) == "table" then
+				flatten(v)
+			else
+				table.insert(result, v)
+			end
+		end
+	end
+	
+	flatten(tbl)
+	return result
+end
+
+local function PrintIndex(k, indexMap)
+	if indexMap ~= nil and type(indexMap) == "table" then
+		local displayValue = indexMap[k]
+		if displayValue ~= nil then
+			return displayValue
+		end
+	end
+	if type(k) == "string" then
+		return '"'..k..'"'
+	else
+		return tostring(k)
+	end
+end
+
+---Print a value or table (recursive).
+---@param o table
+---@param indexMap table
+---@param innerOnly boolean
+---@return string
+local function DumpTable(o, indexMap, innerOnly, recursionLevel)
+	if recursionLevel == nil then recursionLevel = 0 end
+	if type(o) == 'table' then
+		local s = '{ '
+		for k,v in pairs(o) do
+			if innerOnly == true then
+				if recursionLevel > 0 then
+					s = s .. ' ['..PrintIndex(k, indexMap)..'] = ' .. DumpTable(v, indexMap, innerOnly, recursionLevel + 1) .. ','
+				else
+					s = s .. ' ['..PrintIndex(k, nil)..'] = ' .. DumpTable(v, indexMap, innerOnly, recursionLevel + 1) .. ','
+				end
+			else
+				s = s .. ' ['..PrintIndex(k, indexMap)..'] = ' .. DumpTable(v, indexMap, innerOnly, recursionLevel + 1) .. ','
+			end
+		end
+		return s .. '} \n'
+	else
+		return tostring(o)
+	end
+end
+
+local function BuildPartyStructure(printDebug)
+	local players = FlattenTable(Osi.DB_IsPlayer:Get(nil))
+	local partyLeaders = {}
+	local highestLevel = 1
+
+	if printDebug then 
+		Ext.Print("[LLXPSCALE:BootstrapServer.lua:BuildPartyStructure] Players: (".. DumpTable(players) ..").")
+	end
+
+	if #players == 1 then
+		local player = CharacterGetHostCharacter()
+		local level = CharacterGetLevel(player)
+		if printDebug then 
+			Ext.Print("[LLXPSCALE:BootstrapServer.lua:BuildPartyStructure] Only one player ("..player..") at level ("..tostring(level)..")")
+		end
+		return player,level
+	end
+
+	local tableEmpty = true
+	for i,v in pairs(players) do
+		local level = CharacterGetLevel(v)
+		if level > highestLevel then highestLevel = level end
+		if tableEmpty then
+			if printDebug then 
+				Ext.Print("[LLXPSCALE:BootstrapServer.lua:BuildPartyStructure] partyLeaders count is 0. Adding to table. ("..v..")")
+			end
+			partyLeaders[v] = {}
+			tableEmpty = false
+		else
+			for leader,members in pairs(partyLeaders) do
+				if v ~= leader then
+					if CharacterIsInPartyWith(leader, v) == 1 then
+						if printDebug then 
+							Ext.Print("[LLXPSCALE:BootstrapServer.lua:BuildPartyStructure] (".. v ..") is in a party with ("..leader..")?")
+						end
+						members[#members+1] = v
+					else
+						if printDebug then 
+							Ext.Print("[LLXPSCALE:BootstrapServer.lua:BuildPartyStructure] (".. v ..") is not in a party with ("..leader..")?")
+						end
+						partyLeaders[v] = {}
+					end
+				end
+			end
+		end
+	end
+	if printDebug then 
+		Ext.Print("[LLXPSCALE:BootstrapServer.lua:BuildPartyStructure] Party leader structure: (".. DumpTable(partyLeaders) ..").")
+	end
+	return partyLeaders,highestLevel
+end
+
+local function LLXPSCALE_GrantPartyExperience(char)
+	local stats = nil
+	local character = Ext.GetCharacter(char)
+	if character ~= nil then
+		stats = character.Stats.Name
+	end
+	if stats == nil then stats = GetStatString(char) end
+	if stats ~= nil then
+		local printDebug = Ext.IsDeveloperMode()
+		local gain = NRD_StatGetInt(stats, "Gain")
+
+		if gain > 0 then
+			local enemyLevel = CharacterGetLevel(char)
+			if printDebug then
+				Ext.Print("[LLXPSCALE:BootstrapServer.lua:LLXPSCALE_Ext_GrantExperience] Granting experience to all players scaled by (" .. tostring(gain) ..") gain. ")
+			end
+			local partyStructure,highestLevel = BuildPartyStructure()
+			if GlobalGetFlag("LLXPSCALE_AlwaysScaleToPlayerLevel") == 0 and enemyLevel < highestLevel then
+				if printDebug then
+					Ext.Print("[LLXPSCALE:BootstrapServer.lua:LLXPSCALE_Ext_GrantExperience] NPC level is lower than party level. Using NPC level for xp scaling. (" .. tostring(highestLevel) .." => "..tostring(enemyLevel)..")")
+				end
+				highestLevel = enemyLevel
+			end
+			if type(partyStructure) == "string" then
+				PartyAddExperience(partyStructure, 1, highestLevel, gain)
+				if printDebug then
+					Ext.Print("[LLXPSCALE:BootstrapServer.lua:LLXPSCALE_Ext_GrantExperience] Granting xp to sole player (" .. tostring(partyStructure) ..") scaled to level ("..tostring(highestLevel).."). ")
+				end
+			else
+				for leader,members in pairs(partyStructure) do
+					PartyAddExperience(leader, 1, highestLevel, gain)
+					if printDebug then
+						Ext.Print("[LLXPSCALE:BootstrapServer.lua:LLXPSCALE_Ext_GrantExperience] Granting xp to party of (" .. tostring(leader) ..") scaled to level ("..tostring(highestLevel).."). ")
+					end
+					return true
+				end
+			end
+		else
+			if printDebug then
+				Ext.Print("[LLXPSCALE:BootstrapServer.lua:LLXPSCALE_Ext_GrantExperience] Skipping experience for (" .. tostring(char) ..") since Gain is 0. ")
+			end
+		end
+	end
+	return false
+end
+
+Ext.NewCall(LLXPSCALE_GrantPartyExperience, "LLXPSCALE_GrantPartyExperience", "(CHARACTERGUID)_Character")
+--Ext.NewQuery(LLXPSCALE_GrantPartyExperience, "LLXPSCALE_GrantPartyExperience", "[in](CHARACTERGUID)_Character")
