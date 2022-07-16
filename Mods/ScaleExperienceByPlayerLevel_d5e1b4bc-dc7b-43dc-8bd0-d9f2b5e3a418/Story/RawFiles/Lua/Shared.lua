393199ad60a0a2c1f860c33fedf30e653fa06c85
LaughingLeader <laughingleader@gmail.com>
2022-07-16 09:01:51 -0500

    Re-enable ModuleResume (fixed in latest extender)

--- Mods/ScaleExperienceByPlayerLevel_d5e1b4bc-dc7b-43dc-8bd0-d9f2b5e3a418/Story/RawFiles/Lua/Shared.lua
+++ Mods/ScaleExperienceByPlayerLevel_d5e1b4bc-dc7b-43dc-8bd0-d9f2b5e3a418/Story/RawFiles/Lua/Shared.lua
@@ -1,9 +1,8 @@
 local function DisableGain()
 	--- Disables default Gain scaling.
 	Ext.Stats.SetLevelScaling("Character", "Gain", function(gain,level) return 0 end)
-	--Ext.Utils.Print("[LLXPSCALE:DisableGain] Registered Gain scale override.")
+	Ext.Utils.Print("[LLXPSCALE:DisableGain] Registered Gain scale override.")
 end
 
 Ext.Events.ModuleLoading:Subscribe(DisableGain)
---TODO causes a crash in v56 as of 7/14/2022
---Ext.Events.ModuleResume:Subscribe(DisableGain)
+Ext.Events.ModuleResume:Subscribe(DisableGain)
